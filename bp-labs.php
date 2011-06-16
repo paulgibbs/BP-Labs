035bdaa856c8e7960ea737187ee9d9e96d9b0cab
Paul Gibbs <djpaul@gmail.com>
2011-06-16 21:51:25 +0000

    Regular placeholder files. Also, first commit!

---
+++ bp-labs.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: BP Labs
+Plugin URI: http://buddypress.org/community/groups/bp-labs/
+Description: BP Labs offers experimental BuddyPress features for testing and feedback, and to give you new shiny things to play with on your site. These are my personal, unofficial, core experiments, and anything may happen to them.
+Version: 1.0
+Author: Paul Gibbs
+Author URI: http://byotos.com
+Network: true
+Domain Path: /languages/
+Text Domain: bpl
+License: GPL3
+
+"BP Labs"
+Copyright (C) 2011 Paul Gibbs
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+
+?>
