979927c55cde44b36ff49b2d821f5cc341bc54c3
Paul Gibbs <djpaul@gmail.com>
2011-06-18 10:39:36 +0000

    First version of beaker complete

--- beakers/class-bplabs-beaker.php
+++ beakers/class-bplabs-beaker.php
@@ -10,5 +10,47 @@
 if ( !defined( 'ABSPATH' ) )
 	exit;
 
+/**
+ * Each component of BP Labs needs to extend and implement this beaker class.
+ *
+ * I haven't built pieces of plugin functionality in this pattern before. This, too, is an experiment!
+ *
+ * @since 1.0
+ */
+abstract class BPLabs_Beaker {
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0
+	 */
+	function __construct() {
+		add_action( 'init', array( 'BPLabs', 'enqueue_script' ) );
+		add_action( 'init', array( 'BPLabs', 'enqueue_style' ) );
+		add_action( 'init', array( 'BPLabs', 'register_actions' ) );
+	}
 
+	/**
+	 * Enqueue your javascript here
+	 *
+	 * @since 1.0
+	 */
+	function enqueue_script() {
+	}
+
+	/**
+	 * Enqueue your CSS here
+	 *
+	 * @since 1.0
+	 */
+	function enqueue_style() {
+	}
+
+	/**
+	 * Hook 
+	 *
+	 * @since 1.0
+	 */
+	function register_actions() {
+	}
+}
 ?>
