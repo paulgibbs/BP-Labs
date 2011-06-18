2971152c415fee52afcd3c76b28e45a129f80618
Paul Gibbs <djpaul@gmail.com>
2011-06-18 00:00:05 +0000

    Created placeholder beakers and fleshed out main loader

---
+++ beakers/class-bplabs-beaker.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The Beaker class is a simple container for enqueuing CSS, Javascript and hooking into actions.
+ *
+ * @package BP_Labs
+ * @subpackage Beaker
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) )
+	exit;
+
+
+?>
--- bp-labs.php
+++ bp-labs.php
@@ -1,31 +1,95 @@
 <?php
-/*
-Plugin Name: BP Labs
-Plugin URI: http://buddypress.org/community/groups/bp-labs/
-Description: BP Labs offers experimental BuddyPress features for testing and feedback, and to give you new shiny things to play with on your site. These are my personal, unofficial, core experiments, and anything may happen to them.
-Version: 1.0
-Author: Paul Gibbs
-Author URI: http://byotos.com
-Network: true
-Domain Path: /languages/
-Text Domain: bpl
-License: GPL3
+/**
+ * Plugin Name: BP Labs
+ * Plugin URI: http://buddypress.org/community/groups/bp-labs/
+ * Description: BP Labs contains unofficial and experimental BuddyPress features for testing and feedback. Cake, and grief counselling, will be available at the conclusion of the plugin.
+ * Version: 1.0
+ * Author: Paul Gibbs
+ * Author URI: http://byotos.com
+ * Network: true
+ * Domain Path: /languages/
+ * Text Domain: bpl
+ * License: GPL3
+ */
 
-"BP Labs"
-Copyright (C) 2011 Paul Gibbs
+/**
+ * BP Labs contains unofficial and experimental BuddyPress features for testing and feedback. Cake, and grief counselling, will be available at the conclusion of the plugin.
+ *
+ * "BP Labs"
+ * Copyright (C) 2011 Paul Gibbs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * @package BP_Labs
+ */
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3 as published by
-the Free Software Foundation.
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) )
+	exit;
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+/**
+ * Welcome to the main BP Labs class. Anything and everything happens in here, come on in!
+ *
+ * @since 1.0
+ */
+class BPLabs {
+	/**
+	 * Creates an instance of the BPLabs class, and loads i18n.
+	 *
+	 * Thanks to the Jetpack plugin for the idea with this function.
+	 *
+	 * @return BPLabs object
+	 * @since 1.0
+	 * @static
+	 */
+	function &init() {
+		static $instance = false;
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see http://www.gnu.org/licenses/.
-*/
+		if ( !$instance ) {
+			load_plugin_textdomain( 'bpl', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+			$instance = new BPLabs;
+		}
 
+		return $instance;
+	}
 
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0
+	 */
+	function __construct() {
+		$this->_includes();
+	}
+
+	/**
+	 * Include beakers for science.
+	 *
+	 * @since 1.0
+	 */
+	protected function _includes() {
+		require_once( dirname( __FILE__ ) . '/beakers/class-bplabs-beaker.php' );
+
+		require_once( dirname( __FILE__ ) . '/beakers/class-bplabs-autosuggest.php' );
+		require_once( dirname( __FILE__ ) . '/beakers/class-bplabs-quickadmin.php' );
+
+		//require_once( dirname( __FILE__ ) . '/beakers/class-bplabs-swish.php' );
+		// I bet you want to know what Swish is going to do.
+
+		do_action( 'bplabs_includes' );
+	}
+}
+add_action( 'bp_include', array( 'BPLabs', 'init' ) );
+
+// The cake is a lie.
 ?>
