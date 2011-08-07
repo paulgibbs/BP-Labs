abc92f0e5f7ddacd0719e1abf7ff9c5c2d9d726d
Paul Gibbs <djpaul@gmail.com>
2011-08-07 18:18:56 +0000

    whitespace fix

--- beakers/js/autosuggest.dev.js
+++ beakers/js/autosuggest.dev.js
@@ -2,7 +2,7 @@ jQuery(document).ready( function() {
 	// @mentions autosuggest
 	if (jQuery.fn.mentions) {
 		jQuery('#comment').mentions();                                                                            // comments
-		jQuery('#whats-new').mentions({ resultsbox : '#whats-new-options', resultsbox_position : 'prepend' });    //  What's new / status update
+		jQuery('#whats-new').mentions({ resultsbox : '#whats-new-options', resultsbox_position : 'prepend' });    // What's new / status update
 		jQuery('#message_content').mentions({ resultsbox : '#message_content', resultsbox_position : 'after' });  // messaging (body)
 		jQuery('#topic_text').mentions({ resultsbox : '#topic_text', resultsbox_position : 'after' });            // forums (bbPress standalone)
 		jQuery('#reply_text').mentions({ resultsbox : '#reply_text', resultsbox_position : 'after' });            // forums (bbPress standalone)
