0df371d3a825b642b4bc971b8b4b51bb42cf6537
Paul Gibbs <djpaul@gmail.com>
2011-12-15 20:05:35 +0000

    Whoops, put back file accidentally removed in r46

---
+++ beakers/class-bplabs-beaker.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The Beaker class is a simple container for enqueuing CSS, Javascript and hooking into actions.
+ *
+ * @package BP_Labs
+ * @subpackage Beaker
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) )
+	exit;
+
+/**
+ * Each component of BP Labs needs to extend and implement this beaker class.
+ *
+ * I haven't built pieces of plugin functionality in this pattern before. This, too, is an experiment; for science!
+ *
+ * @since 1.0
+ */
+abstract class BPLabs_Beaker {
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0
+	 */
+	public function __construct() {
+		if ( is_admin() )
+			return;
+
+		add_action( 'init', array( $this, 'enqueue_script' ) );
+		add_action( 'init', array( $this, 'enqueue_style' ) );
+
+		$this->register_actions();
+	}
+
+	/**
+	 * Enqueue your javascript here
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_script() {
+	}
+
+	/**
+	 * Enqueue your CSS here
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_style() {
+	}
+
+	/**
+	 * Hook into actions here. The true starting point for a new experiment.
+	 *
+	 * @since 1.0
+	 */
+	protected function register_actions() {
+	}
+}
+?>
