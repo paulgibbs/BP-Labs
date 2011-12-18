45c082f5f64cab8c0ccca77362821b6dbd661644
Paul Gibbs <djpaul@gmail.com>
2011-12-18 18:39:14 +0000

    Hooked up like JS. Still no activity stream disaply.

--- beakers/class-bplabs-like.php
+++ beakers/class-bplabs-like.php
@@ -65,6 +65,9 @@ class BPLabs_Like extends BPLabs_Beaker {
 
 		// AJAX handler
 		add_action( 'wp_ajax_bpl-like', array( 'BPLabs_Like', 'ajax_reciever' ) );
+
+		// Activity integration
+		add_action( 'bp_register_activity_actions', array( 'BPLabs_Like', 'register_activity_actions' ) );
 	}
 
 	/**
@@ -76,9 +79,96 @@ class BPLabs_Like extends BPLabs_Beaker {
 	 * @since 1.3
 	 */
 	public static function is_like_enabled() {
-		if ( bp_is_blog_page() && is_single() || defined( 'DOING_AJAX' ) && DOING_AJAX )
+		if ( ( bp_is_blog_page() && is_single() || defined( 'DOING_AJAX' ) && DOING_AJAX ) && is_user_logged_in() )
 			return true;
 
+		return false;
+	}
+
+	/**
+	 * Has the specified post already been liked by any user?
+	 *
+	 * @param int $post_id Optional; if not set, get ID from Post template loop.
+	 * @return mixed False if post has NOT been liked, otherwise returns the existing Like's activity ID
+	 * @since 1.3
+	 */
+	public static function has_post_been_liked( $post_id = 0 ) {
+		// If this is not set, get ID from Post template loop.
+		if ( ! $post_id )
+			$post_id = get_the_ID();
+
+		// Double-check post is set, and that the Activity component is active
+		if ( ! $post_id || ! bp_is_active( 'activity' ) )
+			return false;
+
+		$activity = bp_activity_get( array(
+			'filter' => array(
+				'action'     => 'bpl_like',  // Action to filter on
+				'object'     => 'bpl_like',  // Type to filter on
+				'primary_id' => $post_id,    // Post ID to filter on
+			),
+			'max'    => 1,
+			'spam'   => 'all',
+		) );
+
+		$activity_id = false;
+
+		if ( ! empty( $activity['activities'] ) )
+			$activity_id = $activity['activities'][0]->id;
+
+		return $activity_id;
+	}
+
+	/**
+	 * Has the specified user already liked the specified post?
+	 *
+	 * @param int $post_id Optional; if not set, get ID from Post template loop.
+	 * @param int $user_id Optional; if not set, get current logged in user's ID.
+	 * @return mixed False if post has NOT been liked, otherwise returns the Like's activity ID
+	 * @since 1.3
+	 */
+	public static function has_user_liked_post( $post_id = 0, $user_id = 0 ) {
+		// If this is not set, get ID from Post template loop.
+		if ( ! $post_id )
+			$post_id = get_the_ID();
+
+		// If this is not set, get ID from current logged in user
+		if ( ! $user_id && is_user_logged_in() )
+			$user_id = bp_loggedin_user_id();
+
+		// Double-check both IDs are set, and that the Activity component is active
+		if ( ! $post_id || ! $user_id || ! bp_is_active( 'activity' ) )
+			return false;
+
+		$activity = bp_activity_get( array(
+			'filter' => array(
+				'action'     => 'bpl_like',  // Action to filter on
+				'object'     => 'bpl_like',  // Type to filter on
+				'primary_id' => $post_id,    // Post ID to filter on
+			),
+			'max'    => 1,
+			'spam'   => 'all',
+		) );
+
+		if ( ! empty( $activity['activities'] ) )
+			$activity = $activity['activities'][0];
+		else
+			return false;
+
+		// Check if the creator of the original Like activity is the current user
+		if ( $user_id == $activity->user_id )
+			return $activity->id;
+
+		// Loop through the meta, and compare recorded user IDs
+		$metas = (array) bp_activity_get_meta( $activity->id, 'bpl_like' );
+		foreach ( $metas as $meta ) {
+
+			// User has Liked this post, but didn't create the original Like.
+			if ( $user_id == $meta[0] )
+				return $activity->id;
+		}
+
+		// If we're down here, the user hasn't Liked this post... yet?
 		return false;
 	}
 
@@ -100,21 +190,42 @@ class BPLabs_Like extends BPLabs_Beaker {
 		$like_url = remove_query_arg( array( 'like', '_wpnonce' ), $_SERVER['REQUEST_URI'] );
 		$like_url = add_query_arg( array( 'like' => get_the_ID(), '_wpnonce' => $nonce ), $like_url );
 
+		// Set defaults for add_menu()
+		$class = '';
+		$title = '<span class="ab-icon"></span><span class="ab-label">' . _x( 'Like', 'toolbar like button label', 'bpl' ) . '</span>';
+
+		// Has the user has already liked this post?
+		$activity_id = BPLabs_Like::has_user_liked_post();
+		if ( $activity_id )
+			$class = 'liked';
+
+		// Get the Like count
+		$activity_id = BPLabs_Like::has_post_been_liked();
+		if ( $activity_id ) {
+			$meta  = (array) bp_activity_get_meta( $activity_id, 'bpl_like' );
+
+			if ( 'liked' == $class )
+				$title = '<span class="ab-icon"></span><span class="ab-label">' . number_format_i18n( count( $meta ) ) . '</span>';
+			else
+				$title = '<span class="ab-icon"></span><span class="ab-label">' .sprintf( _x( '%s likes', 'toolbar like button alternative label', 'bpl' ), number_format_i18n( count( $meta ) ) ) . '</span>';
+		}
+
 		// Add the top-level Group Admin button
 		$wp_admin_bar->add_menu( array(
 			'href'  => $like_url,
 			'id'    => 'bpl-likebutton',
 			'meta'  => array(
+				'class' => $class,
 				'title' => _x( 'Like this Post', 'toolbar like button title', 'bpl' ),
 			),
-			'title' => '<span class="ab-icon"></span><span class="ab-label">' . _x( 'Like', 'toolbar like button label', 'bpl' ) . '</span>',
+			'title' => $title,
 		) );
 	}
 
 	/**
 	 * AJAX receiver for Likes.
 	 *
-	 * @return string '-1' if something is missing. '0' if post doesn't exist. Or 'n', where n = new/existing Like ID
+	 * @return string '-1' if something is missing. '0' if post doesn't exist. Or 'n', where n = number of Likes for the $post_id
 	 * @since 1.3
 	 */
 	public function ajax_reciever() {
@@ -131,6 +242,10 @@ class BPLabs_Like extends BPLabs_Beaker {
 		if ( ! bp_is_active( 'activity' ) )
 			die( '-1' );
 
+		// Check user is logged in
+		if ( ! is_user_logged_in() )
+			die( '-1' );
+
 		// Check post exists
 		$post = get_post( $post_id );
 		if ( is_null( $post ) )
@@ -140,29 +255,46 @@ class BPLabs_Like extends BPLabs_Beaker {
 		// if ( ! current_user_can( 'bpl_like', $post_id ) )
 		//	die( '-1' );
 
-		// Does a Like for this item exist?
-		$activity = bp_activity_get( array(
-			'filter' => array(
-				'object'       => 'bpl_like',  // Type to filter on
-				'action'       => 'like',      // Action to filter on
-				'primary_id'   => $post_id,    // Post ID to filter on
-			),
-			'max'    => 1,
-			'spam'   => 'all',
-		) );
+		// Does a Like for this item exist? (for any user)
+		$activity_id = BPLabs_Like::has_post_been_liked( $post_id );
 
-		// Update existing Like meta
-		if ( ! empty( $activity['activities'] ) ) {
-			$activity = $activity['activities'][0];
+		// Like already exists
+		if ( $activity_id ) {
+			// Add new Like to meta
+			$meta   = (array) bp_activity_get_meta( $activity_id, 'bpl_like' );
+			$meta[] = array( bp_loggedin_user_id(), bp_core_current_time() );
 
+			// Update meta
+			bp_activity_update_meta( $activity_id, 'bpl_like', $meta );
 
+			// Get Likes total
+			$likes_count = count( $meta );
 
 		// Add new Like
 		} else {
+			$activity_id = bp_activity_add( array(
+				'component' => 'bpl_like',
+				'item_id'   => $post_id,
+				'type'      => 'bpl_like',
+				'user_id'   => bp_loggedin_user_id(),
+			) );
+
+			$likes_count = 1;
 		}
 
-		// Send response
-		exit( 000000 ); // put new like ID here
+		// Send Likes total back
+		exit( (string) number_format_i18n( $likes_count ) );
+	}
+
+	/**
+	 * Register the activity stream actions for Likes
+	 *
+	 * @global object $bp BuddyPress global settings
+	 * @since 1.3
+	 */
+	function register_activity_actions() {
+		global $bp;
+		bp_activity_set_action( $bp->activity->id, 'bpl_like', __( 'Liked a Post', 'bpl' ) );
 	}
 }
 new BPLabs_Like();
--- beakers/js/like.dev.js
+++ beakers/js/like.dev.js
@@ -17,6 +17,19 @@ bplLike = {
 	},
 
 	/**
+	 * Manages the spinner animation when we're waiting for AJAX results
+	 *
+	 * @since 1.3
+	 */
+	start_spinner : function () {
+		var link = $( '#wp-admin-bar-bpl-likebutton a' );
+
+		if ( ! link.data( 'stop_pulse' ) ) {
+			link.children( ".ab-icon" ).animate( { opacity: 0.2 }, 400, 'linear' ).animate( { opacity: 1 }, 400, 'linear', bplLike.start_spinner );
+		}
+	},
+
+	/**
 	 * Send a Like
 	 *
 	 * @since 1.3
@@ -34,17 +47,29 @@ bplLike = {
 		data.action   = 'bpl-like';
 		data.like     = decodeURIComponent( like[1].replace( /\+/g, ' ' ) );
 
-		// Make the AJAX call
+		// Send the Like request
 		$.ajax( {
-			data    : data,
-			type    : 'POST',
-			url     : ajaxurl,
+			data : data,
+			type : 'POST',
+			url  : ajaxurl,
 
+			beforeSend : function ( e ) {
+				// Hide the 'Like this Post' text
+				$( '#wp-admin-bar-bpl-likebutton a' ).children( '.ab-label' ).fadeOut( 200 )
+				.end()
+				.data( 'stop_pulse', false ).blur();
 
-complete : function (e) {
-console.log(e.responseText);
-}
+				// Start the spinner
+				bplLike.start_spinner();
+			},
 
+			complete : function ( e ) {
+				// Stop the spinner, set the Like count to the menu item label, and fade it back in.
+				$( '#wp-admin-bar-bpl-likebutton' ).addClass( 'liked' )
+				.children( 'a' ).data( 'stop_pulse', true )
+				.children( '.ab-label' ).html( e.responseText )
+				.fadeIn( 200 );
+			}
 		} );
 
 		return false;
