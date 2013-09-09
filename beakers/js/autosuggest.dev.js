a235b8bcc49423cc4e5e2603dacc7abbfe8bb658
r-a-y <r-a-y@gmx.com>
2013-09-09 12:03:53 -0700

    Autosuggest: Add support for activity comments.

--- beakers/js/autosuggest.dev.js
+++ beakers/js/autosuggest.dev.js
@@ -6,5 +6,23 @@ jQuery(document).ready( function() {
 		jQuery('#message_content').mentions({ resultsbox : '#message_content', resultsbox_position : 'after' });  // messaging (body)
 		jQuery('#topic_text').mentions({ resultsbox : '#topic_text', resultsbox_position : 'after' });            // forums (bbPress standalone)
 		jQuery('#reply_text').mentions({ resultsbox : '#reply_text', resultsbox_position : 'after' });            // forums (bbPress standalone)
+
+		// activity comments
+		jQuery('div.activity').on( 'click', function(event) {
+			var target = jQuery(event.target);
+
+			if ( target.hasClass('acomment-reply') || target.parent().hasClass('acomment-reply') ) {
+				if ( target.parent().hasClass('acomment-reply') )
+					target = target.parent();
+
+				var id = target.attr('id');
+				ids = id.split('-');
+
+				jQuery('#ac-form-' + ids[2] + ' textarea').mentions({ resultsbox : '#ac-form-' + ids[2] + ' textarea', resultsbox_position : 'after' });
+
+				return false;
+			}
+		});
+
 	}
 });
